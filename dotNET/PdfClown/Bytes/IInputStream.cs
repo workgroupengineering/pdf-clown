0aaa71f1b4e6d0c7fa0494e873c3aae941ae7471
Alexandr Vassilyev <alexandr_vslv@mail.ru>
2024-10-01 18:59:28 +0500

    Refactoring docs

--- dotNET/PdfClown/Bytes/IInputStream.cs
+++ dotNET/PdfClown/Bytes/IInputStream.cs
@@ -30,126 +30,89 @@ using System;
 
 namespace PdfClown.Bytes
 {
-    /**
-      <summary>Input stream interface.</summary>
-    */
+    /// <summary>Input stream interface.</summary>
     public interface IInputStream : IStream, IDataWrapper
     {
-        /**
-          <summary>Gets/Sets the byte order.</summary>
-        */
+        /// <summary>Gets/Sets the byte order.</summary>
         ByteOrderEnum ByteOrder { get; set; }
 
         bool IsAvailable { get; }
 
-        /**
-          <summary>Gets the hash representation of the sequence.</summary>
-        */
+        /// <summary>Gets the hash representation of the sequence.</summary>
         int GetHashCode();
 
-        /**
-          <summary>Gets the pointer position.</summary>
-        */
+        /// <summary>Gets the pointer position.</summary>
         long Position { get; set; }
 
-        /**
-          <summary>Reads a sequence of bytes.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-          <param name="data">Target byte array.</param>
-        */
+        /// <summary>Reads a sequence of bytes.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
+        /// <param name="data">Target byte array.</param>
         int Read(byte[] data);
 
-        /**
-          <summary>Reads a sequence of bytes.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-          <param name="data">Target byte array.</param>
-        */
+        /// <summary>Reads a sequence of bytes.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
+        /// <param name="data">Target byte array.</param>
         int Read(Span<byte> data);
 
-        /**
-          <summary>Reads a sequence of bytes.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-          <param name="data">Target byte array.</param>
-          <param name="offset">Location in the byte array at which storing begins.</param>
-          <param name="length">Number of bytes to read.</param>
-        */
+        /// <summary>Reads a sequence of bytes.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
+        /// <param name="data">Target byte array.</param>
+        /// <param name="offset">Location in the byte array at which storing begins.</param>
+        /// <param name="length">Number of bytes to read.</param>
         int Read(byte[] data, int offset, int length);
 
-        /**
-          <summary>Reads a byte.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads a byte.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         int ReadByte();
 
         int PeekByte();
 
         byte PeekUByte(int offset);
-        /**
-          <summary>Reads an integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+
+        /// <summary>Reads an integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         int ReadInt32();
 
-        /**
-          <summary>Reads an unsigned integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads an unsigned integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         uint ReadUInt32();
 
-        /**
-          <summary>Reads a variable-length integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-          <param name="length">Number of bytes to read.</param>
-        */
+        /// <summary>Reads a variable-length integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
+        /// <param name="length">Number of bytes to read.</param>
         int ReadInt(int length);
 
-        /**
-          <summary>Reads the next line of text.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads the next line of text.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         string ReadLine();
 
-        /**
-          <summary>Reads a short integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads a short integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         short ReadInt16();
 
-        /**
-          <summary>Reads an unsigned short integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads an unsigned short integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         ushort ReadUInt16();
 
-        /**
-          <summary>Reads a bit integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads a bit integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         long ReadInt64();
 
-        /**
-          <summary>Reads an unsigned bit integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads an unsigned bit integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         ulong ReadUInt64();
 
-        /**
-          <summary>Reads a signed byte integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads a signed byte integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         sbyte ReadSByte();
 
-        /**
-          <summary>Reads a unsigned byte integer.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-        */
+        /// <summary>Reads a unsigned byte integer.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
         byte ReadUByte();
 
-        /**
-          <summary>Reads a string.</summary>
-          <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
-          <param name="length">Number of bytes to read.</param>
-        */
+        /// <summary>Reads a string.</summary>
+        /// <remarks>This operation causes the stream pointer to advance after the read data.</remarks>
+        /// <param name="length">Number of bytes to read.</param>
         string ReadString(int length);
 
         Span<byte> ReadSpan(int length);
@@ -162,14 +125,10 @@ namespace PdfClown.Bytes
 
         uint ReadBits(int count);
 
-        /**
-          <summary>Sets the pointer absolute position.</summary>
-        */
+        /// <summary>Sets the pointer absolute position.</summary>
         long Seek(long position);
 
-        /**
-          <summary>Sets the pointer relative position.</summary>
-        */
+        /// <summary>Sets the pointer relative position.</summary>
         long Skip(long offset);
 
         int Mark();
